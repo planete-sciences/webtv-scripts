8d4f17ae7d3e8c83966b76a3308076f0cda9813e
Pierre Boutillier <pierre.boutillier@planete-sciences.org>
2014-11-18 23:47:26 +0100

    first success ogv + webm

---
+++ webcam2webmweb.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+## Quand c'est cassé ou qu'on modifie quelque chose, on le dit sur gitolite
+## ou l'on prévient l'auteur pierre.boutillier@PLASCI afin d'en laisser la trace
+
+# Copyright 2014 Planète Sciences
+# This file is part of webtv-plasci
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+assure_commande () { ## assure_commande commande paquet
+which $1 > /dev/null ||\
+ { echo "Commande \"$1\" introuvable, il faut surement installer le paquet $2."
+ exit 3 ; }
+}
+
+assure_commande gst-launch-1.0 gstreamer1.0-tools
+
+[ $# -eq 4 ] || { echo "Usage: $0 nom_serveur_icecast port_serveur_icecast mdp_icecast prefix_mountpoint" ; echo "Capture le flux depuis la webcam et l'envoi au serveur icecast en ogv et webm." ; exit 2 ; }
+
+[ "$TERM" = "screen" ] || { echo "Il faut lancer ce script en utilisant screen pour que la diffusion survive aux deconnections." ; exit 4 ; }
+
+
+gst-launch-1.0 v4l2src ! queue ! videorate ! "video/x-raw,framerate=25/1" ! tee name=video \
+	       video. ! queue ! videoscale ! "video/x-raw,height=512,width=640" ! theoraenc ! oggmux ! queue ! \
+	       shout2send ip=$1 port=$2 password=$3 mount=$4".ogv" \
+	       video. ! queue ! videoscale ! "video/x-raw,height=190,width=240" ! vp8enc ! webmmux ! queue ! \
+	       shout2send ip=$1 port=$2 password=$3 mount=$4".webm"
